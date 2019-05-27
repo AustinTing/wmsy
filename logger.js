37492bbe66be88e0044d04b0edc769e14ff5511c
CellBody <austintingwork@gmail.com>
2019-05-27 10:56:45 +0800

    Update logger

--- logger.js
+++ logger.js
@@ -1,14 +1,25 @@
 const log4js = require('log4js')
 // trace, debug, info, warn, error, fatal
-log4js.configure({
-  appenders: {
-    file: { type: 'dateFile', layout: { type: 'colored' }, filename: `./log/${(new Date()).getFullYear()}.log`, keepFileExt: true, pattern: '-MM-dd' },
-    console: { type: 'stdout', layout: { type: 'colored' } }
-  },
-  categories: {
-    default: { appenders: ['file', 'console'], level: process.env.LOG_LEVEL }
-  }
-})
+if (process.env.STATE === 'production') {
+  log4js.configure({
+    appenders: {
+      file: { type: 'dateFile', layout: { type: 'colored' }, filename: `./log/${(new Date()).getFullYear()}.log`, keepFileExt: true, pattern: '-MM-dd' },
+      console: { type: 'stdout', layout: { type: 'colored' } }
+    },
+    categories: {
+      default: { appenders: ['file', 'console'], level: process.env.LOG_LEVEL }
+    }
+  })
+} else {
+  log4js.configure({
+    appenders: {
+      console: { type: 'stdout', layout: { type: 'colored' } }
+    },
+    categories: {
+      default: { appenders: ['console'], level: process.env.LOG_LEVEL }
+    }
+  })
+}
 
 module.exports.getLogger = (wherejs) => {
   const logger = log4js.getLogger(wherejs)
